1c5abb3dd4b36e456feed7543257b0f79363dc9e
Nicholas <nicholasnogueira12@gmail.com>
2022-02-17 22:01:26 -0300

    Mais anotações e inicio de alguns projetos

---
+++ consoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace consoleApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int numDeVezes = 5;
+
+      for (int i = 0; i < numDeVezes; i++)
+      {
+          Console.WriteLine($"Bem-vindo ao curso de .NET {i+1}");
+      }
+    }
+  }
+}
